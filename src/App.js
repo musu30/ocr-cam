9713a927269c5f6be057af7dbe40e637957de354
musammil30 <musammil@ysquaretechnology.com>
2022-06-22 08:51:04 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import { useEffect, useState } from "react";
+import { createWorker } from "tesseract.js";
+import "./App.css";
+function App() {
+  const [ocr, setOcr] = useState("");
+  const [imageData, setImageData] = useState(null);
+  const [progress, setProgress] = useState(0)
+  const worker = createWorker({
+    logger: (m) => {
+      // console.log(m);
+      setProgress(parseInt(m.progress * 100));
+     
+    },
+  });
+  const convertImageToText = async () => {
+    if (!imageData) return;
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text },
+    } = await worker.recognize(imageData);
+    setOcr(text);
+  };
+
+  useEffect(() => {
+    convertImageToText();
+  }, [imageData]);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    if(!file)return;
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageDataUri = reader.result;
+      // console.log({ imageDataUri });
+      setImageData(imageDataUri);
+    };
+    reader.readAsDataURL(file);
+  }
+  return (
+    <div className="App">
+      <div>
+        <p>Choose an Image</p>
+        <input
+          type="file"
+          name=""
+          id=""
+          capture="environment"
+          onChange={handleImageChange}
+          accept="image/*"
+        />
+      </div>
+      {progress < 100 && progress > 0 && <div>
+        <div className="progress-label">Progress ({progress}%)</div>
+        <div className="progress-bar">
+          <div className="progress" style={{width: `${progress}%`}} ></div>
+        </div>
+      </div>}
+      <div className="display-flex">
+        <img src={imageData} alt="" srcset="" />
+        <p>{ocr}</p>
+      </div>
+    </div>
+  );
+}
+export default App;
